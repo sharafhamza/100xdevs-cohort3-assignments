f78351fd6984bdce2f3f59c93d4b4624c7bdeda7
Sharaf Al Hamza <kingsharaf14@gmail.com>
2024-09-18 01:31:05 +0600

    changes

--- Web Development/week-6-authentication/index.js
+++ Web Development/week-6-authentication/index.js
@@ -1,81 +1,12 @@
 const express = require("express");
 
 const app = express();
-
+var jwt = require("jsonwebtoken");
 app.use(express.json());
 
 const users = [];
-const generateToken = () => {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-  let token = "";
-  for (let i = 0; i < 30; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-};
+const JWT_SECRET = "USER_APP";
+
 app.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -98,8 +29,12 @@ app.post("/signin", (req, res) => {
   );
 
   if (user) {
-    const token = generateToken();
-    user.token = token;
+    const token = jwt.sign(
+      {
+        username: user.username,
+      },
+      JWT_SECRET
+    );
     res.send({
       token: token,
     });
@@ -111,4 +46,21 @@ app.post("/signin", (req, res) => {
   console.log(users);
 });
 
+app.get("/me", (req, res) => {
+  const token = req.headers.authorization;
+  const userDetails = jwt.verify(token, JWT_SECRET);
+
+  const username = userDetails.username;
+  const user = users.find((user) => user.username === username);
+
+  if (user) {
+    res.send({
+      username: user.username,
+    });
+  } else {
+    res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+});
 app.listen(3000);
