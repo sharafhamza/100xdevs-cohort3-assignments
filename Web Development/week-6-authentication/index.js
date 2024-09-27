b17930fceab27ecb842a7e678bbaaf8ae63ed6e3
Sharaf Al Hamza <kingsharaf14@gmail.com>
2024-09-27 22:43:49 +0600

    changes

--- Web Development/week-6-authentication/index.js
+++ Web Development/week-6-authentication/index.js
@@ -5,28 +5,12 @@ const JWT_SECRET = "Hellohamza";
 app.use(express.json());
 const users = [];
 
-const auth = (req, res, next) => {
-  const token = req.headers.token;
-  const decodedData = jwt.verify(token, JWT_SECRET);
-
-  if (decodedData.username) {
-    req.username = decodedData.username;
-    next();
-  } else {
-    res.json({
-      message: "You are not logged in",
-    });
-  }
-};
-
 app.post("/signup", (req, res) => {
   const username = req.body.username;
-  const email = req.body.email;
   const password = req.body.password;
 
   users.push({
     username,
-    email,
     password,
   });
   res.send({
@@ -35,42 +19,4 @@ app.post("/signup", (req, res) => {
   console.log(users);
 });
 
-app.post("/signin", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  let findUser = null;
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].username === username && users[i].password === password) {
-      findUser = users[i];
-    }
-  }
-  console.log(findUser);
-
-  if (findUser) {
-    const token = jwt.sign(
-      {
-        username: users.username,
-        password: username.password,
-      },
-      JWT_SECRET
-    );
-    res.send({
-      token: token,
-    });
-  } else {
-    res.send({
-      message: "User isn't found",
-    });
-  }
-});
-
-app.get("/me", function (req, res) {
-  const token = req.headers.token; // jwt
-  const decodedInformation = jwt.verify(token, JWT_SECRET); // {username: "harkirat@gmail.com"}
-  const unAuthDecodedinfo = jwt.decode(token); // {username: "harkirat@gmail.com"}
-  const username = decodedInformation.username;
-  let foundUser = null;
-});
-
-app.listen(4000);
+app.listen(3000);
